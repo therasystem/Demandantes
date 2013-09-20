0cc70da5dc78650f5484870736342489fc68ba02
therasystem <gabriel@therasystem.com.br>
2013-09-20 14:32:04 -0300

    solicitante.exe

--- demandantes/visao/solicitante.exe.php
+++ demandantes/visao/solicitante.exe.php
@@ -23,26 +23,98 @@ if ($metodoG == 1 || $metodoP == 1) {
     extract($_POST, EXTR_PREFIX_SAME, "wddx");
 
     $dadosDemandante->setCodigo($codigo);
-    $dadosDemandante->setNome($CARGO);
+    $dadosDemandante->setCPF($CPF);
+    $dadosDemandante->setNOME($NOME);
+    $dadosDemandante->setNOMEMAE($NOMEMAE);
+    $dadosDemandante->setRG($RG);
+    $dadosDemandante->setDTEMISSAO($DTEMISSAO);
+    $dadosDemandante->setIDESTADOIDENT($IDESTADOIDENT);
+    $dadosDemandante->setIDORGAOEMISSORIDENT($IDORGAOEMISSORIDENT);
+    $dadosDemandante->setNUMTITULOELEITOR($NUMTITULOELEITOR);
+    $dadosDemandante->setNUMZONAELEITOR($NUMZONAELEITOR);
+    $dadosDemandante->setNUMSECAOELEITOR($NUMSECAOELEITOR);
+    $dadosDemandante->setIDPAISNASC($IDPAISNASC);
+    $dadosDemandante->setIDNACIONALIDADENASC($IDNACIONALIDADENASC);
+    $dadosDemandante->setIDESTADONASC($IDESTADONASC);
+    $dadosDemandante->setIDMUNICIPIONASC($IDMUNICIPIONASC);
+    $dadosDemandante->setDTNASC($DTNASC);
+    $dadosDemandante->setNUMNIS($NUMNIS);
+    $dadosDemandante->setTELRESID($TELRESID);
+    $dadosDemandante->setTELCOMERC($TELCOMERC);
+    $dadosDemandante->setTELMOVEL($TELMOVEL);
+    $dadosDemandante->setEMAIL1($EMAIL1);
+    $dadosDemandante->setEMAIL2($EMAIL2);
+    $dadosDemandante->setIDSEXO($IDSEXO);
+    $dadosDemandante->setIDESTADOCIVIL($IDESTADOCIVIL);
+    $dadosDemandante->setCPFCONJUGE($CPFCONJUGE);
+    $dadosDemandante->setIDGRAUINSTRUCAO($IDGRAUINSTRUCAO);
+    $dadosDemandante->setIDSITMERCADOTRABALHO($IDSITMERCADOTRABALHO);
+    $dadosDemandante->setIDOCUPACAO($IDOCUPACAO);
+    $dadosDemandante->setDTADMISTRAB($DTADMISTRAB);
+    $dadosDemandante->setIDDEFICIENCIATIPO($IDDEFICIENCIATIPO);
+    $dadosDemandante->setIDRESTLOCOMOCAO($IDRESTLOCOMOCAO);
+    $dadosDemandante->setIDBENGOV($IDBENGOV);
+    $dadosDemandante->setVALORBENEF($VALORBENEF);
+    $dadosDemandante->setDTINIBENEF($DTINIBENEF);
+    $dadosDemandante->setIDPROGGOV($IDPROGGOV);
+    $dadosDemandante->setVALORPROG($VALORPROG);
+    $dadosDemandante->setDTINIPROG($DTINIPROG);
+    $dadosDemandante->setIDENTIDADE($IDENTIDADE);
 
     $dadosDemandante->Alterar();
     $msg = "Registro alterado com sucesso!";
-    $config->ConfirmaOperacao("visao/cargo.frm.php", $msg);
+    $config->ConfirmaOperacao("visao/solicitante.frm.php", $msg);
 } else if ($metodoG == 2 || $metodoP == 2) {
     // DELETAR
     $cod = $_GET['idcargo'];
     $dadosDemandante->Excluir($cod);
     $msg = "Registro excluido com sucesso!";
-    $config->ConfirmaOperacao("visao/cargo.frm.php", $msg);
+    $config->ConfirmaOperacao("visao/solicitante.frm.php", $msg);
 } else {
     // SALVAR
     extract($_POST, EXTR_PREFIX_SAME, "wddx");
 
-    $dadosDemandante->setNome($CARGO);
+    $dadosDemandante->setCPF($CPF);
+    $dadosDemandante->setNOME($NOME);
+    $dadosDemandante->setNOMEMAE($NOMEMAE);
+    $dadosDemandante->setRG($RG);   
+    $dadosDemandante->setDTEMISSAO($DTEMISSAO);
+    $dadosDemandante->setIDESTADOIDENT($IDESTADOIDENT);
+    $dadosDemandante->setIDORGAOEMISSORIDENT($IDORGAOEMISSORIDENT);
+    $dadosDemandante->setNUMTITULOELEITOR($NUMTITULOELEITOR);
+    $dadosDemandante->setNUMZONAELEITOR($NUMZONAELEITOR);
+    $dadosDemandante->setNUMSECAOELEITOR($NUMSECAOELEITOR);
+    $dadosDemandante->setIDPAISNASC($IDPAISNASC);
+    $dadosDemandante->setIDNACIONALIDADENASC($IDNACIONALIDADENASC);
+    $dadosDemandante->setIDESTADONASC($IDESTADONASC);
+    $dadosDemandante->setIDMUNICIPIONASC($IDMUNICIPIONASC);
+    $dadosDemandante->setDTNASC($DTNASC);
+    $dadosDemandante->setNUMNIS($NUMNIS);
+    $dadosDemandante->setTELRESID($TELRESID);
+    $dadosDemandante->setTELCOMERC($TELCOMERC);
+    $dadosDemandante->setTELMOVEL($TELMOVEL);
+    $dadosDemandante->setEMAIL1($EMAIL1);
+    $dadosDemandante->setEMAIL2($EMAIL2);
+    $dadosDemandante->setIDSEXO($IDSEXO);
+    $dadosDemandante->setIDESTADOCIVIL($IDESTADOCIVIL);
+    $dadosDemandante->setCPFCONJUGE($CPFCONJUGE);
+    $dadosDemandante->setIDGRAUINSTRUCAO($IDGRAUINSTRUCAO);
+    $dadosDemandante->setIDSITMERCADOTRABALHO($IDSITMERCADOTRABALHO);
+    $dadosDemandante->setIDOCUPACAO($IDOCUPACAO);
+    $dadosDemandante->setDTADMISTRAB($DTADMISTRAB);
+    $dadosDemandante->setIDDEFICIENCIATIPO($IDDEFICIENCIATIPO);
+    $dadosDemandante->setIDRESTLOCOMOCAO($IDRESTLOCOMOCAO);
+    $dadosDemandante->setIDBENGOV($IDBENGOV);
+    $dadosDemandante->setVALORBENEF($VALORBENEF);
+    $dadosDemandante->setDTINIBENEF($DTINIBENEF);
+    $dadosDemandante->setIDPROGGOV($IDPROGGOV);
+    $dadosDemandante->setVALORPROG($VALORPROG);
+    $dadosDemandante->setDTINIPROG($DTINIPROG);
+    $dadosDemandante->setIDENTIDADE($IDENTIDADE);
 
     $dadosDemandante->Salvar();
     $msg = "Registro inserido com sucesso!";
-    $config->ConfirmaOperacao("visao/cargo.frm.php", $msg);
+    $config->ConfirmaOperacao("visao/solicitante.frm.php", $msg);
 }
  
 ?>
